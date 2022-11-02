933145b47b50d5a87fac506462814a38f1834486
Aman <eyeon4840@gmail.com>
2022-11-02 12:21:59 +0530

    added gson converter

--- app/src/main/java/com/example/fruit_app_kotlin_task/network/FruitApiService.kt
+++ app/src/main/java/com/example/fruit_app_kotlin_task/network/FruitApiService.kt
@@ -1,23 +1,22 @@
 package com.example.fruit_app_kotlin_task.network
 
 import com.example.fruit_app_kotlin_task.response.FruitModel
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://mercadosagricolaspr.com/farmer-dev/apis/"
 
-
-private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory())
-    .build()
+//
+//private val moshi = Moshi.Builder()
+//    .add(KotlinJsonAdapterFactory())
+//    .build()
 
 
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addConverterFactory(GsonConverterFactory.create())
     .baseUrl(BASE_URL)
     .build()
 
